c4401e153a3797d16321fabc0f0352832eb67f7d
Teo Wei Shen <tws4793@gmail.com>
2021-06-08 16:22:24 +0800

    refactor

--- src/l4meta/duplicates.py
+++ src/l4meta/duplicates.py
@@ -1,40 +1,40 @@
-"""Find duplicates"""
+"""Find duplicates."""
 
+import os
 import stat
+import sys
 import hashlib
 
 
-def findDup(parentFolder):
+def find_duplicates(directory):
+    """Find duplicates in directory."""
     # Dups in format {hash:[names]}
-    dups = {}
-    for dirName, subdirs, fileList in os.walk(parentFolder):
-        print('Scanning %s...' % dirName)
-        for filename in fileList:
+    duplicates = {}
+    for dir_name, _, filenames in os.walk(directory):
+        print('Scanning %s...' % dir_name)
+        for filename in filenames:
             # Get the path to the file
-            path = os.path.join(dirName, filename)
-            # Calculate hash
-            file_hash = hashfile(path)
+            path = os.path.join(dir_name, filename)
+            file_hash = calculate_hash(path)
             # Add or append the file path
-            if file_hash in dups:
-                dups[file_hash]['path'].append(path)
+            if file_hash in duplicates:
+                duplicates[file_hash]['path'].append(path)
             else:
-                dups[file_hash] = {}
-                dups[file_hash]['path'] = [path]
+                duplicates[file_hash] = {}
+                duplicates[file_hash]['path'] = [path]
                 if os.path.isfile(path):
-                    dups[file_hash]['size'] = os.stat(path)[stat.ST_SIZE]
-    return dups
+                    duplicates[file_hash]['size'] = os.stat(path)[stat.ST_SIZE]
+    return duplicates
+ 
 
-
-# Joins two dictionaries
-def joinDicts(dict1, dict2):
+def join_dictionaries(dict1, dict2):
+    """Join two dictionaries."""
     for key in dict2.keys():
-        if key in dict1:
-            dict1[key] = dict1[key] + dict2[key]
-        else:
-            dict1[key] = dict2[key]
+        dict1[key] = dict1[key] if key in dict1 else '' + dict2[key]
 
 
-def hashfile(path, blocksize = 65536):
+def calculate_hash(path, blocksize: int = 65536):
+    """Calculate hash of a path."""
     afile = open(path, 'rb')
     hasher = hashlib.md5()
     buf = afile.read(blocksize)
@@ -44,45 +44,46 @@ def hashfile(path, blocksize = 65536):
     afile.close()
     return hasher.hexdigest()
 
-def sizeof_fmt(num, suffix):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+
+def size_format(num, suffix):
+    """Format size."""
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
-def printResults(dict1):
+
+def print_results(dict1):
+    """Print results."""
     results = list(filter(lambda x: len(x['path']) > 1, dict1.values()))
     summ = 0
-    if len(results) > 0:
-        results = sorted(results, key=lambda res: res['size'])
-        print('Duplicates:')
-        print('___________________')
-        for result in results:
-            coef = len(result['path']) - 1
-            summ += result['size'] * coef
-            print('\t%s' % sizeof_fmt(result['size'], 'B'))
-            for subresult in result['path']:
-                print('\t\t%s' % subresult)
-            print('___________________')
-        print('TOTAL SIZE %s' % sizeof_fmt(summ, 'B'))
-
-    else:
+    if len(results) == 0:
         print('No duplicate files found.')
+        return
+    results = sorted(results, key=lambda res: res['size'])
+    print('Duplicates:')
+    print('___________________')
+    for result in results:
+        coef = len(result['path']) - 1
+        summ += result['size'] * coef
+        print('\t%s' % size_format(result['size'], 'B'))
+        for subresult in result['path']:
+            print('\t\t%s' % subresult)
+        print('___________________')
+    print('TOTAL SIZE %s' % size_format(summ, 'B'))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        dups = {}  
-        folders = sys.argv[1:]
-        for i in folders:
-            # Iterate the folders given
-            if os.path.exists(i):
-                # Find the duplicated files and append them to the dups
-                joinDicts(dups, findDup(i))
-            else:
-                print('%s is not a valid path, please verify' % i)
-                sys.exit()
-        printResults(dups)
-    else:
-        print('Usage: python duplicates_finder.py folder or python duplicates_finder.py folder1 folder2 folder3')    
+    if len(sys.argv) <= 0:
+        print('usage: python duplicates.py directory [directory ...]')
+    duplicates = {}
+    directories = sys.argv[1:]
+    for directory in directories:
+        # Iterate the folders given
+        if not os.path.exists(directory):
+            print('%s is not a valid path, please verify' % directory)
+            sys.exit()
+        # Find the duplicated files and append them to the duplicates
+        join_dictionaries(duplicates, find_duplicates(directory))
+    print_results(duplicates)
